71c510c66a5d93f31f95e8230ba3773cd9582e92
Dhar-Koyyana <dharanikoyyana05@gmail.com>
2025-04-20 05:41:00 +0530

    changes

---
+++ demo.py
@@ -0,0 +1,7 @@
+# s = open('demo3.txt', mode='a')
+# # s.write('new demo for life change')  # Writes to the file
+# print(s.write('new'))  # Error: write() expects an argument (a string to write)
+# # print(s.read())  # Error: File is opened in 'w' mode, which doesn't support reading
+# s.close()
+print("hello world")
+# print("new")
